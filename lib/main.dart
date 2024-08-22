998ef2d82cc7f9e3d410247615d0c64e0a60e2a7
Educador <sv77@calabria.com.br>
2024-08-22 16:58:19 -0300

    Criação da tela de login.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Login());
+  runApp(const LoginApp());
 }
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class LoginApp extends StatelessWidget {
+  const LoginApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,67 +13,114 @@ class Login extends StatelessWidget {
       title: 'CookBooks',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         useMaterial3: true,
       ),
-      home: const LoginScreen(title: 'Login'),
+      home: const LoginScreen(),
     );
   }
 }
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key, required this.title});
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
-  final String title;
-
-  @override
-  State<LoginScreen> createState() => _HomeState();
-}
-
-class _HomeState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: Text(widget.title),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/imagem_de_fundo_1920x1080.png'),
-            fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          // Imagem de fundo
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/backgrounds/background_cookbooks.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget> [
-              Image.asset(
-                'images/imagem_logo_culinaria_1920x1080.png',
-                width: 300,
-                height: 300,
+          Center(
+            child: Container(
+              width: 100.0, // Largura do contêiner
+              height: 100.0, // Altura do contêiner (igual à largura para que seja circular)
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50.0) // Raio igual à metade do tamanho para fazer o círculo
               ),
-              const SizedBox(height: 20),
-              const TextField(
-                decoration: InputDecoration(labelText: 'Usuário'),
+            ),
+          ),
+          Column(
+            children: [
+              // Container superior
+              Container(
+                height: MediaQuery.of(context).size.height * 0.35,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40),
+                  ),
+                ),
               ),
-              const TextField(
-                decoration: InputDecoration(labelText: 'Senha'),
-                obscureText: true,
-              ),
-              const SizedBox(height: 20),
-              const ElevatedButton(
-                onPressed: null, // Aqui você adiciona a funcionalidade de login
-                child: Text('Entrar'),
-              ),
-              const SizedBox(height: 20),
-              const ElevatedButton(
-                onPressed: null, // Aqui você adiciona a funcionalidade de login
-                child: Text('Cadastra-se'),
+              const Spacer(),
+              // Container inferior
+              Container(
+                padding: const EdgeInsets.all(20.0),
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    const TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Usuário',
+                        prefixIcon: Icon(Icons.person),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Senha',
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Adicione a funcionalidade de login aqui
+                      },
+                      child: const Text('Entrar'),
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 100, vertical: 15),
+                        textStyle: const TextStyle(fontSize: 18),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {
+                        // Navegação para a tela de cadastro
+                      },
+                      child: const Text(
+                        'Se não tem cadastro ainda, cadastre-se',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
