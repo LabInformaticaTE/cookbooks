896e6d6c6e7f5a72dd1d11923c18e431e5d1ec90
Educador <sv77@calabria.com.br>
2024-09-02 15:03:08 -0300

    Ajustando o layout das telas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:cookbooks/screens/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,100 +10,153 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CookBooks',
       debugShowCheckedModeBanner: false,
+      title: 'CookBooks',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-        useMaterial3: true,
+        primaryColor: Colors.black,
       ),
-      home: const SplashScreen(),
+      home: Home(),
     );
   }
 }
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  State<Home> createState() => _HomeState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(const Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const LoginScreen()),
-      );
-    });
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // Imagem de fundo
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/backgrounds/background_cookbooks.png'), // Caminho da imagem de fundo
-                fit: BoxFit.cover,
-              ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                  'assets/images/backgrounds/background_cookbooks.png'),
+              fit: BoxFit.cover,
             ),
           ),
-          // Conteúdo da tela
-          Center(
+          child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  width: 200.0,
-                  height: 200.0,
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(100.0),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100.0),
-                    child: Image.asset(
-                      'assets/images/icons/logo_cookbooks.png',
-                      fit: BoxFit.cover,
+                Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(20.0),
+                      width: 300.0,
+                      height: 300.0,
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(100.0),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100.0),
+                        child: Image.asset(
+                          'images/logos/logo_cookbooks.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'CookBooks',
-                  style: TextStyle(
-                    fontSize: 90.0,
-                    fontFamily: 'Satisfy',
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                const SizedBox(height: 50),
-                const Text(
-                  'Desenvolvido por: TE laboratório de informática',
-                  style: TextStyle(fontSize: 20.0, color: Colors.black),
-                ),
-                const SizedBox(height: 10),
-                Container(
-                  width: 50.0,
-                  height: 50.0,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(25.0),
-                    child: Image.asset(
-                      'images/icons/logo_informatica.png', // Coloque o caminho do logo do desenvolvedor
-                      fit: BoxFit.cover,
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(100.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "CookBook's",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 80.0,
+                            fontFamily: 'Satisfy',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
                     ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Lógica para a Splash Screen de Login
+                        print('Botão de Login pressionado');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white, // Cor do botão
+                        padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                      ),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          fontFamily: 'Satisfy',
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Lógica para a Splash Screen de Cadastro
+                        print('Botão de Cadastro pressionado');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white, // Cor do botão
+                        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      ),
+                      child: Text(
+                        'Cadastro',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          fontFamily: 'Satisfy',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.all(60),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Desenvolvido por:',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          fontFamily: 'Satisfy',
+                        ),
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(100.0),
+                        child: Image.asset(
+                          'images/logos/logo_lab_informatica.png',
+                          fit: BoxFit.cover,
+                          width: 50,
+                          height: 50,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:cookbooks/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cookbooks/main.dart';
@@ -13,7 +12,7 @@ import 'package:cookbooks/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const LoginScreen());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
